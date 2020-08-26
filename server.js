cc3f6a82d88acf36d11ca593106bb3f22a1f6c35
Nastavshev <nastavshev_yuriy@mail.ru>
2020-08-26 23:09:50 +0300

    autorization + render mainscreen hbs

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express()
 
 const mongoose = require("mongoose");
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const db = require('./db');
+const User = require('./models/user');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -12,6 +14,7 @@ app.set('view engine', 'hbs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({ //библиотека express-session - мидлвер для сессий
@@ -24,7 +27,24 @@ app.use(session({ //библиотека express-session - мидлвер для
 
 module.exports = app
 
+app.get('/', (req, res) => {
+  res.render('signin');
+});
 
+app.post('/admin', async (req, res) => {
+  const { email, password } = req.body;
+  const findUser = await User.findOne({ email });
+  if (findUser) {
+    if (findUser.password === password) {
+      req.session.user = findUser;
+      const users = await User.find();
+      console.log(users);
+      res.render('mainscreen', { users });
+    }
+  } else {
+    res.redirect('/');
+  }
+});
 
 
 
