3de7ea223e3ea605e3c40cfe7b43691f8c1cd741
ИVAN <i9175439504@gmail.com>
2020-08-26 19:00:45 +0300

    express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 
 const mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost:27017/broccoli', {
@@ -7,13 +8,12 @@ mongoose.connect('mongodb://localhost:27017/broccoli', {
   useUnifiedTopology: true
 });
 
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.use(session({ //библиотека express-session - мидлвер для сессий
   secret: 'dfgiodhgosjgopsjgpowejf345345',
@@ -23,13 +23,13 @@ app.use(session({ //библиотека express-session - мидлвер для
 }));
 
 
+app.listen(3333, () => {
+  console.log('work 3333');
+})
+
 module.exports = app
 
 
 
 
 
-
-app.listen(3333, () => {
-  console.log('work 3333');
-})
