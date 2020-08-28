90db34291f8e49ab7ba6bffabefc5e50748c0523
Denis <lamagoshi@gmail.com>
2020-08-28 16:05:05 +0300

    style new person

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -3,27 +3,31 @@ function createCalendar(id, year, month, arr) {
 
   var mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
   var d = new Date(year, mon);
-  let count = 0
+  let count = 0;
 
-  let str = '<th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th>'
-  let table = `<table><tr>${str.repeat(5)}</tr><tr></tr>`
-
+  let str =
+    "<th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th>";
+  let table = `<table><tr>${str.repeat(5)}</tr><tr></tr>`;
 
   // заполнить первый ряд от понедельника
   // и до дня, с которого начинается месяц
   // * * * | 1  2  3  4
   for (var i = 0; i < getDay(d); i++) {
-    table += '<td></td>';
+    table += "<td></td>";
   }
 
   // ячейки календаря с датами
   while (d.getMonth() == mon) {
-
-    table += '<td class="num ' + (arr.includes(d.getDate()) ? 'disabled' : '') + '" >' + d.getDate() + '</td>';
+    table +=
+      '<td class="num ' +
+      (arr.includes(d.getDate()) ? "disabled" : "") +
+      '" >' +
+      d.getDate() +
+      "</td>";
 
     //if они равны меняем класс
 
-    count++
+    count++;
 
     // if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
     //   table += '</tr><tr>';
@@ -35,68 +39,60 @@ function createCalendar(id, year, month, arr) {
   // добить таблицу пустыми ячейками, если нужно
   if (getDay(d) != 0) {
     for (var i = getDay(d); i < count; i++) {
-      table += '<td></td>';
+      table += "<td></td>";
     }
   }
 
   // закрыть таблицу
-  table += '</tr></table>';
-  table += '<button id="btn">add</button>'
+  table += "</tr></table>";
+  table += '<button id="btn">add</button>';
   // только одно присваивание innerHTML
   elem.innerHTML = table;
 }
-let green = document.getElementsByClassName('disabled')
-
-
-
-
+let green = document.getElementsByClassName("disabled");
 
 //green.forEach(el => el.style.background = 'green')
 
-function getDay(date) { // получить номер дня недели, от 0(пн) до 6(вс)
+function getDay(date) {
+  // получить номер дня недели, от 0(пн) до 6(вс)
   var day = date.getDay();
   if (day == 0) day = 7;
   return day - 1;
 }
 
+createCalendar("calendar", 2012, 9, [3, 5, 7, 9]);
 
-createCalendar("calendar", 2012, 9, [3, 5, 7, 9])
+let arr = [];
 
+let text = document.querySelectorAll(".num");
+text.forEach((el) =>
+  el.addEventListener("click", (e) => {
+    if (e.target.classList.contains("disabled")) {
+      alert("нужно сформировать запрос на коллеге, эта дата занята");
+    }
+    e.target.classList.toggle("green");
+    //arr.push(Number(e.target.innerHTML))
 
-
-let arr = []
-
-let text = document.querySelectorAll('.num')
-text.forEach(el => el.addEventListener('click', (e) => {
-
-  if (e.target.classList.contains('disabled')) {
-    alert('нужно сформировать запрос на коллеге, эта дата занята')
-  }
-  e.target.classList.toggle('green')
-  //arr.push(Number(e.target.innerHTML))
-
-  if (arr.indexOf(Number(e.target.innerHTML)) === -1) {
-    arr.push(Number(e.target.innerHTML))
-  } else {
-    let index = arr.indexOf(Number(e.target.innerHTML))
-    //delete arr[index]
-    arr.splice(index, 1)
-  }
-
-
-}))
+    if (arr.indexOf(Number(e.target.innerHTML)) === -1) {
+      arr.push(Number(e.target.innerHTML));
+    } else {
+      let index = arr.indexOf(Number(e.target.innerHTML));
+      //delete arr[index]
+      arr.splice(index, 1);
+    }
+  })
+);
 // не работает на getElementsByClassName
 
-let cd = document.querySelectorAll('.disabled')
+let cd = document.querySelectorAll(".disabled");
 let as = [...cd];
-as.map(el => el.style.background === '' ? el.style.background = 'green' : '')
+as.map((el) =>
+  el.style.background === "" ? (el.style.background = "green") : ""
+);
 
-let btn = document.getElementById('btn')
-btn.addEventListener('click', (e) => {
-
-
-  // +++______________+++вписать нужную ручку, на выходе массив чисел(даты) количество дней 
-
+let btn = document.getElementById("btn");
+btn.addEventListener("click", (e) => {
+  // +++______________+++вписать нужную ручку, на выходе массив чисел(даты) количество дней
   //   fetch('/form', {
   //     method: 'POST',
   //     Headers: {
@@ -105,8 +101,4 @@ btn.addEventListener('click', (e) => {
   //     body: JSON.stringify({ arr:arr, count: count })
   //   })
   // })
-
-
-})
-
-
+});
